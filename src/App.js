61819bfb23abbb615ba416a0a7481cfbde2ac7c0
Akeen Zhong <101462287@georgebrown.ca>
2024-11-28 19:25:41 -0500

    first commit for labtest 2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import GetWeather from './GetWeather'
+
+
+
+function App(){
+
+
+    return (
+        <GetWeather/>
+        );
+    }
+
+export default App;
---
+++ src/GetWeather.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const api = axios.create();
+
+function GetWeather() {
+    const [temperatureData, setTemperatureData] = useState();
+    const [minTempData, setMinTempData] = useState();
+    const [maxTempData, setMaxTempData] = useState();
+    const [city, setCity] = useState();
+    const [winds, setWinds] = useState();
+    const [clouds, setClouds] = useState();
+    const [humidity, setHumidity] = useState();
+    useEffect(() => {
+        axios.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=fe15647dd5e3cce75aacc72cdd96df5e")
+            .then((res) => {
+                setTemperatureData(res.data.main.temp);
+                setMinTempData(res.data.main.temp_min);
+                setMaxTempData(res.data.main.temp_max);
+                setCity(res.data.name);
+                setWinds(res.data.wind.speed);
+                setClouds(res.data.clouds.all);
+                setHumidity(res.data.main.humidity);
+            })
+            .catch(e=> console.log(e));
+    }, [])
+
+
+    return (
+        <div className="ouput">
+            <h2>Data from Open Weather</h2>
+            <p>Temprature in: {city}</p>
+            <p>Current Temperature: {temperatureData} degrees Celsius</p>
+            <p>Maximum Temperature: {maxTempData} degrees Celsius</p>
+            <p>Minimum Temperature: {minTempData} degrees Celsius</p>
+            <p>Wind speed: {winds} km/h</p>
+            <p>Cloud coverage: {clouds}%</p>
+            <p>Humidity: {humidity}%</p>
+        </div>
+    );
+}
+
+export default GetWeather;
